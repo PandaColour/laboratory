3460c022b6a8746cbbcbbc524c690346a38a1034
panda.colour <panda.colour@qq.com>
2025-04-22 09:19:08 +0800

    clear folder

--- README.md
+++
@@ -1,8 +0,0 @@
-# laboratory
-我的实验室项目,学习项目
-
-## lecture
-面向对象教学和使用Junit4来做自动化测试
-
-## EatMelon
-各种吃瓜
--- lecture/SmallBank/src/main/java/com/qihoo/smallbank/service/TransactionService.java
+++
@@ -1,8 +0,0 @@
-package com.qihoo.smallbank.service;
-
-import java.math.BigDecimal;
-
-public interface TransactionService {
-    // 定义交易相关的业务方法
-    void transfer(String tranType, String seqNo, String fromAccountCode, String toAccountCode, BigDecimal amount);
-}
